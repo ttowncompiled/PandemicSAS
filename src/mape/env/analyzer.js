671c8a37b5438e9a3e951df5df42471d4aa49329
ttowncompiled <ian.riley.915@gmail.com>
2018-11-10 11:01:14 -0600

    Creates action tree for start action for env player.

--- src/mape/env/analyzer.js
+++ src/mape/env/analyzer.js
@@ -1,4 +1,59 @@
 exports.analysis = (probe) => {
-    console.log('here');
-    return {};
+    let tree = {};
+    tree.root = {
+        id: 'root',
+        links: [],
+    };
+    branchActions(probe, tree.root);
+    return tree;
+};
+
+function branchActions(probe, root) {
+    branchStartAction(probe, root);
+};
+
+function branchStartAction(probe, root) {
+    if (probe.round !== 1) {
+        return;
+    }
+    let action = {
+        id: 'Start',
+        links: [],
+    };
+    root.links.push(action);
+    for (let i = 0; i < 3; i++) {
+        action = branchDrawInfectCard(probe, action);
+        for (let j = 0; j < 3; j++) {
+            action = branchInfectCity(probe, action);
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        action = branchDrawInfectCard(probe, action);
+        for (let j = 0; j < 2; j++) {
+            action = branchInfectCity(probe, action);
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        action = branchDrawInfectCard(probe, action);
+        action = branchInfectCity(probe, action);
+    }
+    return action;
+};
+
+function branchDrawInfectCard(probe, root) {
+    let action = {
+        id: 'Draw Infect Card',
+        links: [],
+    };
+    root.links.push(action);
+    return action;
+};
+
+function branchInfectCity(probe, root) {
+    let action = {
+        id: 'Infect City',
+        links: [],
+    };
+    root.links.push(action);
+    return action;
 };
