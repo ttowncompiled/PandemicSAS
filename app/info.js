64294037c5306fd4b0ef8c0755e0fa3008e2639c
ttowncompiled <ian.riley.915@gmail.com>
2018-11-13 18:46:31 -0600

    Adds info handler for player, pawn, and round.

--- app/info.js
+++ app/info.js
@@ -3,12 +3,29 @@ let infoUpdate = null;
 
 (() => {
 
+    let active_player = $('#active-player');
+    let active_pawn = $('#active-pawn');
+    let round = $('#round');
+
     infoInit = (data) => {
         console.log(data);
+        setStaticInfo(data);
+        updateDynamicInfo(data);
     };
 
     infoUpdate = (data) => {
         console.log(data);
+        updateDynamicInfo(data);
+    };
+
+    function setStaticInfo(data) {
+
+    };
+
+    function updateDynamicInfo(data) {
+        active_player.html(data.player);
+        active_pawn.html(data.pawn);
+        round.html(data.round);
     };
 
 })();
